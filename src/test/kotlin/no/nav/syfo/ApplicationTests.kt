85580ed5c9a2740e7d761f207b150372ab01e69e
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-03-15 13:13:01 +0100

    Enable spring application test (#17)

---
+++ src/test/kotlin/no/nav/syfo/ApplicationTests.kt
@@ -0,0 +1,11 @@
+package no.nav.syfo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest(classes = [LocalApplication::class])
+class ApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
