58e506776e9cc7cda73d36626f0c1aa6accac7b4
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-03-26 09:15:41 +0100

    Add endpoints for StartRegistration and create

--- src/main/kotlin/no/nav/syfo/senoppfolging/SenOppfolgingControllerV1.kt
+++ src/main/kotlin/no/nav/syfo/senoppfolging/SenOppfolgingControllerV1.kt
@@ -3,21 +3,29 @@ package no.nav.syfo.senoppfolging
 import jakarta.annotation.PostConstruct
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.syfo.auth.TokenUtil
+import no.nav.syfo.auth.TokenUtil.TokenIssuer.TOKENX
 import no.nav.syfo.auth.TokenValidator
 import no.nav.syfo.logger
+import no.nav.syfo.veilarbregistrering.StartRegistrationDTO
+import no.nav.syfo.veilarbregistrering.VeilarbregistreringClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 
 @Controller
 @RequestMapping("/api/v1")
+@ProtectedWithClaims(issuer = "tokenx", combineWithOr = true, claimMap = ["acr=Level4", "acr=idporten-loa-high"])
 class SenOppfolgingControllerV1(
     @Value("\${MEROPPFOLGING_FRONTEND_CLIENT_ID}")
     val merOppfolgingFrontendClientId: String,
     val tokenValidationContextHolder: TokenValidationContextHolder,
+    val veilarbregistreringClient: VeilarbregistreringClient,
 ) {
     lateinit var tokenValidator: TokenValidator
     private val log = logger()
@@ -27,10 +35,21 @@ class SenOppfolgingControllerV1(
         tokenValidator = TokenValidator(tokenValidationContextHolder, merOppfolgingFrontendClientId)
     }
 
-    @GetMapping("/mer", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("/startregistration", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    @ProtectedWithClaims(issuer = "tokenx", combineWithOr = true, claimMap = ["acr=Level4", "acr=idporten-loa-high"])
-    fun getMer(): String {
-        return "mer"
+    fun startRegistration(): StartRegistrationDTO {
+        tokenValidator.validateTokenXClaims()
+        val token = TokenUtil.getIssuerToken(tokenValidationContextHolder, TOKENX)
+        return veilarbregistreringClient.startRegistration(token)
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    fun create(
+        @RequestBody sykmeldtRegistrering: SykmeldtRegistrering,
+    ) {
+        tokenValidator.validateTokenXClaims()
+        val token = TokenUtil.getIssuerToken(tokenValidationContextHolder, TOKENX)
+        veilarbregistreringClient.completeRegistration(token, sykmeldtRegistrering)
     }
 }
---
+++ src/main/kotlin/no/nav/syfo/senoppfolging/SykmeldtRegistrering.kt
@@ -0,0 +1,72 @@
+package no.nav.syfo.senoppfolging
+
+data class SykmeldtRegistrering(
+    val besvarelse: Besvarelse,
+    val teksterForBesvarelse: List<TekstForSporsmal>,
+)
+
+data class Besvarelse(
+    val utdanning: UtdanningSvar? = null,
+    val utdanningBestatt: UtdanningBestattSvar? = null,
+    val utdanningGodkjent: UtdanningGodkjentSvar? = null,
+    val andreForhold: AndreForholdSvar? = null,
+    val sisteStilling: SisteStillingSvar? = null,
+    val fremtidigSituasjon: FremtidigSituasjonSvar? = null,
+    val tilbakeIArbeid: TilbakeIArbeidSvar? = null,
+)
+
+data class TekstForSporsmal(
+    val sporsmalId: String,
+    val sporsmal: String,
+    val svar: String,
+)
+
+enum class UtdanningSvar {
+    INGEN_UTDANNING,
+    GRUNNSKOLE,
+    VIDEREGAENDE_GRUNNUTDANNING,
+    VIDEREGAENDE_FAGBREV_SVENNEBREV,
+    HOYERE_UTDANNING_1_TIL_4,
+    HOYERE_UTDANNING_5_ELLER_MER,
+    INGEN_SVAR,
+}
+
+enum class UtdanningBestattSvar {
+    JA,
+    NEI,
+    INGEN_SVAR,
+}
+
+enum class UtdanningGodkjentSvar {
+    JA,
+    NEI,
+    VET_IKKE,
+    INGEN_SVAR,
+}
+
+enum class AndreForholdSvar {
+    JA,
+    NEI,
+    INGEN_SVAR,
+}
+
+enum class FremtidigSituasjonSvar {
+    SAMME_ARBEIDSGIVER,
+    SAMME_ARBEIDSGIVER_NY_STILLING,
+    NY_ARBEIDSGIVER,
+    USIKKER,
+    INGEN_PASSER,
+}
+
+enum class TilbakeIArbeidSvar {
+    JA_FULL_STILLING,
+    JA_REDUSERT_STILLING,
+    USIKKER,
+    NEI,
+}
+
+enum class SisteStillingSvar {
+    HAR_HATT_JOBB,
+    HAR_IKKE_HATT_JOBB,
+    INGEN_SVAR,
+}
