117b59808a39ab857a4cc92cf94cf8f62651d92c
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-03-26 14:30:50 +0100

    Fix property name

--- src/main/kotlin/no/nav/syfo/senoppfolging/StatusDTO.kt
+++ src/main/kotlin/no/nav/syfo/senoppfolging/StatusDTO.kt
@@ -3,6 +3,6 @@ package no.nav.syfo.senoppfolging
 import no.nav.syfo.veilarbregistrering.StartRegistrationType
 
 data class StatusDTO(
-    val registreringType: StartRegistrationType,
+    val registrationType: StartRegistrationType,
     val isSykmeldt: Boolean,
 )
