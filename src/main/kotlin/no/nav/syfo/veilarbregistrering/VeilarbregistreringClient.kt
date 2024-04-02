3f230f79b97a73e3888b46f42b9ab73775529d26
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-04-02 10:22:34 +0200

    Revert "Debug logging"
    This reverts commit c7be2158e8ec0e129de3827fff51cdde67b0b372.

--- src/main/kotlin/no/nav/syfo/veilarbregistrering/VeilarbregistreringClient.kt
+++ src/main/kotlin/no/nav/syfo/veilarbregistrering/VeilarbregistreringClient.kt
@@ -60,15 +60,11 @@ class VeilarbregistreringClient(
             )
         val httpEntity = createHttpEntity(exchangedToken, senOppfolgingRegistrering)
 
-        log.info("Are going to send this stuff to veilarbregistrering $httpEntity")
-
-        /*
         try {
             RestTemplate().postForEntity("$baseUrl${VEILARBREGISTRERING_COMPLETE_PATH}", httpEntity, String::class.java)
         } catch (e: RestClientResponseException) {
             handleException(e, httpEntity)
         }
-         */
     }
 
     private fun createHttpEntity(
