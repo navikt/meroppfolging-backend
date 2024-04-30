b64d270e3a3d7cb48e7bd0497597f5c626aed823
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2024-04-30 14:39:35 +0200

    Cannot respond if already responded last 3 months

--- src/main/kotlin/no/nav/syfo/senoppfolging/AlreadyRespondedException.kt
+++ src/main/kotlin/no/nav/syfo/senoppfolging/AlreadyRespondedException.kt
@@ -1,8 +1,7 @@
-package no.nav.syfo.senoppfolging;
+package no.nav.syfo.senoppfolging
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @ResponseStatus(value = HttpStatus.CONFLICT, reason = "User has already responded in the last 3 months")
-public class AlreadyRespondedException extends RuntimeException {
-}
+class AlreadyRespondedException : RuntimeException()
