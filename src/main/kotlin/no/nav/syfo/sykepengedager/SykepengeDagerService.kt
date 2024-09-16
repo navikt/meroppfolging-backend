3f46d3c84fd0bb4684be008b998236013f965f24
AudunSorheim <audun.sorheim@nav.no>
2024-09-16 12:37:12 +0200

    wip

--- src/main/kotlin/no/nav/syfo/sykepengedager/SykepengeDagerService.kt
+++ src/main/kotlin/no/nav/syfo/sykepengedager/SykepengeDagerService.kt
@@ -6,23 +6,6 @@ import org.springframework.stereotype.Service
 class SykepengeDagerService(
     private val sykepengeDagerDAO: SykepengeDagerDAO,
 ) {
-
-
-    // Flytt til jobb-kode
-//    fun shouldSendVarsel(sykepengeDagerDTO: SykepengeDagerDTO): Boolean {
-//        val hasLimitedRemainingSykedager: Boolean = sykepengeDagerDTO.gjenstaendeSykedager < gjenstaendeSykedagerLimit
-//        val maxDateIsEqualToOrMoreThanTwoWeeksAway: Boolean =
-//            sykepengeDagerDTO.maksDato.isAfter(LocalDate.now().plusDays(maxDateLimit))
-//        val hasAlreadySentVarsel: Boolean = varselService.getUtsendtVarsel(sykepengeDagerDTO.fnr) != null
-//        val isBrukerYngreEnn67Ar: Boolean = pdlClient.isBrukerYngreEnnGittMaxAlder(sykepengeDagerDTO.fnr, 67)
-//        val hasActiveSykmelding = true // implement this
-//
-//        return hasLimitedRemainingSykedager &&
-//            maxDateIsEqualToOrMoreThanTwoWeeksAway &&
-//            !hasAlreadySentVarsel &&
-//            isBrukerYngreEnn67Ar
-//    }
-
     fun processSykepengeDagerRecord(sykepengeDagerDTO: SykepengeDagerDTO) {
         sykepengeDagerDAO.storeSykepengeDager(
             fnr = sykepengeDagerDTO.fnr,
