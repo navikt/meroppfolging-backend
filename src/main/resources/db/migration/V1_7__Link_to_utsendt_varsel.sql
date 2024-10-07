f1be334bf04ea3b97ba46ab0cc68642007ce60e9
AudunSorheim <audun.sorheim@nav.no>
2024-10-07 14:58:48 +0200

    Fix flyway numberings

--- src/main/resources/db/migration/V1_7__Link_to_utsendt_varsel.sql
+++ src/main/resources/db/migration/V1_7__Link_to_utsendt_varsel.sql
@@ -1,1 +1,8 @@
-ALTER TABLE FORM_RESPONSE ADD COLUMN UTSENDT_VARSEL_UUID UUID;
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM information_schema.columns
+                       WHERE table_name='form_response'
+                         AND column_name='utsendt_varsel_uuid') THEN
+            ALTER TABLE form_response ADD COLUMN utsendt_varsel_uuid UUID;
+        END IF;
+    END $$;
