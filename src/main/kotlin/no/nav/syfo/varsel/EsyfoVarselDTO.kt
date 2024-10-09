2775dda287b43ccec455fd0d7043c00a1b6ab18d
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-10-09 15:16:36 +0200

    Change datatype (#115)

--- src/main/kotlin/no/nav/syfo/varsel/EsyfoVarselDTO.kt
+++ src/main/kotlin/no/nav/syfo/varsel/EsyfoVarselDTO.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.varsel
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.io.Serializable
+import java.time.LocalDateTime
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME)
 sealed interface EsyfovarselHendelse : Serializable {
@@ -21,3 +22,29 @@ data class ArbeidstakerHendelse(
 enum class HendelseType {
     SM_MER_VEILEDNING,
 }
+
+data class VarselData(
+    val journalpost: VarselDataJournalpost? = null,
+    val narmesteLeder: VarselDataNarmesteLeder? = null,
+    val motetidspunkt: VarselDataMotetidspunkt? = null,
+    val aktivitetskrav: VarselDataAktivitetskrav? = null,
+)
+
+data class VarselDataJournalpost(
+    val uuid: String,
+    val id: String?,
+)
+
+data class VarselDataNarmesteLeder(
+    val navn: String?,
+)
+
+data class VarselDataMotetidspunkt(
+    val tidspunkt: LocalDateTime,
+)
+
+data class VarselDataAktivitetskrav(
+    val sendForhandsvarsel: Boolean,
+    val enableMicrofrontend: Boolean,
+    val extendMicrofrontendDuration: Boolean,
+)
