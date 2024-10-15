c32fb744a3bfa6f4dadbb1106891e6b41db97ad8
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-10-15 13:09:44 +0200

    Fix DB queries, add tests.

--- src/main/kotlin/no/nav/syfo/varsel/database/SendingDueDateDAO.kt
+++ src/main/kotlin/no/nav/syfo/varsel/database/SendingDueDateDAO.kt
@@ -6,7 +6,9 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
 import java.sql.Timestamp
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 @Repository
 class SendingDueDateDAO(
@@ -15,20 +17,23 @@ class SendingDueDateDAO(
     fun persistSendingDueDate(merOppfolgingVarselDTO: MerOppfolgingVarselDTO) {
         val insertStatement = """
             INSERT INTO SENDING_DUE_DATE (
-                person_ident,
-                utbetaling_id,
                 sykmelding_id,
+                utbetaling_id,
+                person_ident,
                 last_day_for_sending
-            ) VALUES (:person_ident, :utbetaling_id, :sykmelding_id, :last_day_for_sending)
-            ON CONFLICT (person_ident, utbetaling_id, sykmelding_id, last_day_for_sending) DO NOTHING 
+            ) VALUES (:sykmelding_id,:utbetaling_id, :person_ident, :last_day_for_sending)
+            ON CONFLICT (person_ident, utbetaling_id, sykmelding_id) DO NOTHING 
             ;
         """.trimIndent()
-
         val parameters = MapSqlParameterSource()
-            .addValue("person_ident", merOppfolgingVarselDTO.personIdent)
-            .addValue("utbetaling_id", merOppfolgingVarselDTO.utbetalingId)
             .addValue("sykmelding_id", merOppfolgingVarselDTO.sykmeldingId)
-            .addValue("last_day_for_sending", Timestamp.valueOf(LocalDateTime.now().plusDays(2)))
+            .addValue("utbetaling_id", merOppfolgingVarselDTO.utbetalingId)
+            .addValue("person_ident", merOppfolgingVarselDTO.personIdent)
+            .addValue(
+                "last_day_for_sending",
+                Timestamp.valueOf(LocalDateTime.of(LocalDate.now(), LocalTime.NOON).plusDays(2))
+            )
+
         namedParameterJdbcTemplate.update(insertStatement, parameters)
     }
 
@@ -41,13 +46,13 @@ class SendingDueDateDAO(
         SELECT *
         FROM SENDING_DUE_DATE
         WHERE person_ident = :person_ident AND sykmelding_id = :sykmelding_id
-        ORDER BY created_at DESC
+        ORDER BY last_day_for_sending DESC
         """.trimIndent()
 
         val parameters = MapSqlParameterSource()
             .addValue("person_ident", personIdent)
-            .addValue("utbetalingId", utbetalingId)
-            .addValue("sykmeldingId", sykmeldingId)
+            .addValue("utbetaling_id", utbetalingId)
+            .addValue("sykmelding_id", sykmeldingId)
 
         return namedParameterJdbcTemplate.query(selectStatement, parameters) { rs, _ -> rs.getDueDate() }.firstOrNull()
     }
