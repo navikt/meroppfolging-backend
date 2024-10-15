1d9ab9c5188046a930b53fa82aeab76ba24c26c9
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-10-15 09:19:16 +0200

    allow all users pilot in dev (#119)

--- src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/BehandlendeEnhet.kt
+++ src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/BehandlendeEnhet.kt
@@ -3,7 +3,6 @@ package no.nav.syfo.behandlendeenhet.domain
 const val OVRE_EIKER_ENHETSNUMMER = "0624"
 const val ASKER_ENHETSNUMMER = "0220"
 const val NORDRE_AKER_ENHETSNUMMER = "0331"
-const val TEST_ENHETSNUMMER = "0314"
 
 data class BehandlendeEnhet(
     var enhetId: String,
@@ -15,5 +14,5 @@ fun BehandlendeEnhet.isPilot(isProd: Boolean): Boolean {
         return listOf(OVRE_EIKER_ENHETSNUMMER, ASKER_ENHETSNUMMER, NORDRE_AKER_ENHETSNUMMER).contains(this.enhetId)
     }
 
-    return listOf(TEST_ENHETSNUMMER).contains(this.enhetId)
+    return true
 }
