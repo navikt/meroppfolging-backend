88a9b068ac900c9260c2d25019803ceb43cbb8cf
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-10-16 12:19:34 +0200

    Add due date for sending SSPS (#118)
    * Add dao and logic for storing due date
    
    * Refactor sending SSPS code
    
    * Refactor sending SSPS code
    
    * Fix DB queries, add tests.

--- src/main/kotlin/no/nav/syfo/varsel/SendSSPSVarselJobb.kt
+++ src/main/kotlin/no/nav/syfo/varsel/SendSSPSVarselJobb.kt
@@ -2,15 +2,18 @@ package no.nav.syfo.varsel
 
 import no.nav.syfo.leaderelection.LeaderElectionClient
 import no.nav.syfo.logger
+import no.nav.syfo.varsel.database.SendingDueDateDAO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.time.LocalDateTime
 
 @Component
 class SendSSPSVarselJobb(
     private val varselService: VarselService,
     private val leaderElectionClient: LeaderElectionClient,
     @Value("\${toggle.ssps.varselutsending}") private var varselutsendingEnabledForEnvironment: Boolean,
+    private var sendingDueDateDAO: SendingDueDateDAO,
 ) {
     private val log = logger()
     private val logName = "[${SendSSPSVarselJobb::class.simpleName}]"
@@ -23,23 +26,50 @@ class SendSSPSVarselJobb(
 
         log.info("$logName Starter jobb")
 
-        var antallVarslerSendt = 0
-
         val varslerToSendToday = varselService.findMerOppfolgingVarselToBeSent()
         log.info("$logName Planlegger å sende ${varslerToSendToday.size} varsler totalt")
 
-        varslerToSendToday.forEach {
-            try {
-                varselService.sendMerOppfolgingVarsel(it)
-                antallVarslerSendt++
-            } catch (e: RuntimeException) {
-                log.error("$logName Feil i utsending av varsel. ${e.message}", e)
-            }
-        }
+        val antallVarslerSendt = sendVarslerWithHandling(varslerToSendToday)
 
         log.info("$logName Sendte $antallVarslerSendt varsler")
         log.info("$logName Avslutter jobb")
 
         return antallVarslerSendt
+    }
+
+    private fun sendVarslerWithHandling(varsler: List<MerOppfolgingVarselDTO>): Int {
+        var antallVarslerSendt = 0
+
+        varsler.forEach { varsel ->
+            try {
+                sendVarsel(varsel)
+                antallVarslerSendt++
+            } catch (e: RuntimeException) {
+                handleSendVarselError(varsel, e)
+            }
+        }
+
+        return antallVarslerSendt
+    }
+
+    private fun sendVarsel(varsel: MerOppfolgingVarselDTO) {
+        varselService.sendMerOppfolgingVarsel(varsel)
+        sendingDueDateDAO.deleteSendingDueDate(varsel.sykmeldingId)
+    }
+
+    private fun handleSendVarselError(varsel: MerOppfolgingVarselDTO, e: RuntimeException) {
+        log.error("$logName Feil i utsending av varsel. ${e.message}", e)
+
+        val dueDate = sendingDueDateDAO.getSendingDueDate(varsel.personIdent, varsel.sykmeldingId, varsel.utbetalingId)
+
+        if (dueDate == null) {
+            sendingDueDateDAO.persistSendingDueDate(varsel)
+        } else if (dueDate.isDueDatePassed()) {
+            log.error("[TOO MANY DAYS]: SSPS varsel must be sent before $dueDate!")
+        }
+    }
+
+    private fun LocalDateTime.isDueDatePassed(): Boolean {
+        return this.isEqual(LocalDateTime.now())
     }
 }
---
+++ src/test/kotlin/no/nav/syfo/varsel/database/SendingDueDateDAOTest.kt
@@ -0,0 +1,94 @@
+package no.nav.syfo.varsel.database
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import no.nav.syfo.LocalApplication
+import no.nav.syfo.varsel.MerOppfolgingVarselDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.kafka.test.context.EmbeddedKafka
+import java.time.LocalDate
+
+@EmbeddedKafka
+@TestConfiguration
+@SpringBootTest(classes = [LocalApplication::class])
+class SendingDueDateDAOTest : DescribeSpec() {
+    @Autowired
+    private lateinit var sendingDueDateDAO: SendingDueDateDAO
+
+    @Autowired
+    lateinit var jdbcTemplate: JdbcTemplate
+
+    init {
+        extension(SpringExtension)
+
+        beforeTest {
+            jdbcTemplate.execute("TRUNCATE TABLE SENDING_DUE_DATE CASCADE")
+        }
+
+        val personIdent = "12345678910"
+        val utbetalingId = "123"
+        val sykmeldingId = "321"
+
+        val dto = MerOppfolgingVarselDTO(
+            personIdent,
+            utbetalingId,
+            sykmeldingId,
+        )
+
+        it("Store due date") {
+            sendingDueDateDAO.persistSendingDueDate(dto)
+
+            val dueDate = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+
+            dueDate shouldNotBe null
+            dueDate?.toLocalDate() shouldBe LocalDate.now().plusDays(2)
+        }
+
+        it("Inserting duplicate dto should not cause exception") {
+            sendingDueDateDAO.persistSendingDueDate(dto)
+
+            val dueDate = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+
+            dueDate shouldNotBe null
+            dueDate?.toLocalDate() shouldBe LocalDate.now().plusDays(2)
+        }
+
+        it("Delete record") {
+            sendingDueDateDAO.persistSendingDueDate(dto)
+
+            val dueDate = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+
+            dueDate shouldNotBe null
+            dueDate?.toLocalDate() shouldBe LocalDate.now().plusDays(2)
+
+            sendingDueDateDAO.deleteSendingDueDate(sykmeldingId)
+
+            val dueDateAfterDelete = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            dueDateAfterDelete shouldBe null
+        }
+
+        it("Deleting duplicate dto should not cause exception") {
+            sendingDueDateDAO.persistSendingDueDate(dto)
+
+            val dueDate = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+
+            dueDate shouldNotBe null
+            dueDate?.toLocalDate() shouldBe LocalDate.now().plusDays(2)
+
+            sendingDueDateDAO.deleteSendingDueDate(sykmeldingId)
+            val dueDateAfterDelete = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            dueDateAfterDelete shouldBe null
+            sendingDueDateDAO.deleteSendingDueDate(sykmeldingId)
+            val dueDateAfterDelete2 = sendingDueDateDAO.getSendingDueDate(personIdent, sykmeldingId, utbetalingId)
+            dueDateAfterDelete2 shouldBe null
+        }
+    }
+}
