932c5024a7bb29b26f5f83a53905f5f7f7f0b1d7
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-10-18 13:33:03 +0200

    Fix varsel utsending (#124)
    * Call esyfovarsel with AAD token
    
    * Fetch max date from db instead of external call
    
    * Remove dependencies
    
    * Add krr i nais
    
    * Refactoring

--- src/main/kotlin/no/nav/syfo/syfoopppdfgen/PdfgenService.kt
+++ src/main/kotlin/no/nav/syfo/syfoopppdfgen/PdfgenService.kt
@@ -12,6 +12,7 @@ import no.nav.syfo.senoppfolging.v2.domain.FremtidigSituasjonSvar
 import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingQuestionV2
 import no.nav.syfo.senoppfolging.v2.domain.behovForOppfolging
 import no.nav.syfo.senoppfolging.v2.domain.fremtidigSituasjonSvar
+import no.nav.syfo.sykepengedagerinformasjon.database.SykepengedagerInformasjonDAO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -23,6 +24,7 @@ class PdfgenService(
     val behandlendeEnhetClient: BehandlendeEnhetClient,
     val dkifClient: DkifClient,
     @Value("\${NAIS_CLUSTER_NAME}") private var clusterName: String,
+    val sykepengedagerInformasjonDAO: SykepengedagerInformasjonDAO,
 ) {
     private val log = logger()
 
@@ -62,29 +64,28 @@ class PdfgenService(
     }
 
     fun getMerVeiledningPdf(personIdent: String): ByteArray {
-        val token = TokenUtil.getIssuerToken(tokenValidationContextHolder, TOKENX)
-        val sykepengerMaxDateResponse = esyfovarselClient.getSykepengerMaxDateResponse(token)
         val behandlendeEnhet = behandlendeEnhetClient.getBehandlendeEnhet(personIdent)
         val isPilotUser = behandlendeEnhet.isPilot(clusterName)
         val isUserReservert = dkifClient.person(personIdent)?.kanVarsles == true
+        val sykepengerInformasjon = sykepengedagerInformasjonDAO.fetchSykepengedagerInformasjonByFnr(personIdent)
 
         return when {
             isUserReservert -> syfooppfpdfgenClient.getMerVeiledningPdf(
                 pdfEndpoint = urlForReservedUsers,
-                utbetaltTom = sykepengerMaxDateResponse?.utbetaltTom,
-                maxDate = sykepengerMaxDateResponse?.maxDate,
+                utbetaltTom = sykepengerInformasjon?.utbetaltTom.toString(),
+                maxDate = sykepengerInformasjon?.forelopigBeregnetSlutt.toString(),
             )
 
             isPilotUser -> syfooppfpdfgenClient.getMerVeiledningPilotUserPdf(
                 pdfEndpoint = urlForDigitalPilotUsers,
-                daysLeft = sykepengerMaxDateResponse?.gjenstaendeSykedager,
-                maxDate = sykepengerMaxDateResponse?.maxDate,
+                daysLeft = sykepengerInformasjon?.gjenstaendeSykedager.toString(),
+                maxDate = sykepengerInformasjon?.forelopigBeregnetSlutt.toString(),
             )
 
             else -> syfooppfpdfgenClient.getMerVeiledningPdf(
                 pdfEndpoint = urlForDigitalUsers,
-                utbetaltTom = sykepengerMaxDateResponse?.utbetaltTom,
-                maxDate = sykepengerMaxDateResponse?.maxDate,
+                utbetaltTom = sykepengerInformasjon?.utbetaltTom.toString(),
+                maxDate = sykepengerInformasjon?.forelopigBeregnetSlutt.toString(),
             )
         }
     }
