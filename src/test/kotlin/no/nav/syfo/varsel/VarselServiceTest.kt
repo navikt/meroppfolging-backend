098bf0e0ac8d0bd4bd88ba2d2e4a68d5ca54bc15
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-10-18 11:44:51 +0200

    test

--- src/test/kotlin/no/nav/syfo/varsel/VarselServiceTest.kt
+++ src/test/kotlin/no/nav/syfo/varsel/VarselServiceTest.kt
@@ -181,7 +181,7 @@ class VarselServiceTest : DescribeSpec() {
 
             it("Should not store utsendt varsel journalforing fails") {
                 every { pdfgenService.getMerVeiledningPdf(any(), any()) } returns ByteArray(1)
-                every { dokarkivClient.postDocumentToDokarkiv(any(), any(), any()) } throws Exception("Help me")
+                every { dokarkivClient.postDocumentToDokarkiv2(any(), any(), any(), any()) } throws Exception("Help me")
 
                 varselService.sendMerOppfolgingVarsel(
                     MerOppfolgingVarselDTO(
@@ -201,7 +201,7 @@ class VarselServiceTest : DescribeSpec() {
             it("Should store utsendt varsel post to dokarkiv OK") {
                 every { pdfgenService.getMerVeiledningPdf(any(), any()) } returns ByteArray(1)
                 every {
-                    dokarkivClient.postDocumentToDokarkiv(any(), any(), any())
+                    dokarkivClient.postDocumentToDokarkiv2(any(), any(), any(), any())
                 } returns DokarkivResponse(null, 1, null, "status", null)
 
                 varselService.sendMerOppfolgingVarsel(
