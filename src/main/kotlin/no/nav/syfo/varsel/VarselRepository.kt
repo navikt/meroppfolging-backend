748d5133bfb7b23310f27b3b55e2874947ac1e50
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-10-22 14:10:56 +0200

    wip

--- src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
+++ src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
-import java.time.LocalDate
+import java.time.LocalDateTime
 import java.util.*
 
 @Repository
@@ -16,34 +16,42 @@ class VarselRepository(
     val maxDateLimit = 14
 
     fun getUtsendtVarsel(personIdent: String): UtsendtVarsel? {
-        val sql = """
-        select uuid, person_ident, utsendt_tidspunkt, utbetaling_id, sykmelding_id
-        from UTSENDT_VARSEL
-        where person_ident = :person_ident
-        AND UTSENDT_TIDSPUNKT > NOW() - INTERVAL '$nyttVarselLimit' DAY
-        """.trimIndent()
+        val sql =
+            """
+            select uuid, person_ident, utsendt_tidspunkt, utbetaling_id, sykmelding_id
+            from UTSENDT_VARSEL
+            where person_ident = :person_ident
+            AND UTSENDT_TIDSPUNKT > NOW() - INTERVAL '$nyttVarselLimit' DAY
+            """.trimIndent()
 
-        val parameters = mapOf(
-            "person_ident" to personIdent,
-        )
+        val parameters =
+            mapOf(
+                "person_ident" to personIdent,
+            )
 
         return namedParameterJdbcTemplate.query(sql, parameters, UtsendtVarselRowMapper()).firstOrNull()
     }
 
-    fun storeUtsendtVarsel(personIdent: String, utbetalingId: String, sykmeldingId: String): UUID {
-        val sql = """
-        INSERT INTO UTSENDT_VARSEL (uuid, person_ident, utsendt_tidspunkt, utbetaling_id, sykmelding_id)
-        VALUES (:uuid, :person_ident, :utsendt_tidspunkt, :utbetaling_id, :sykmelding_id)
-        """.trimIndent()
+    fun storeUtsendtVarsel(
+        personIdent: String,
+        utbetalingId: String,
+        sykmeldingId: String,
+    ): UUID {
+        val sql =
+            """
+            INSERT INTO UTSENDT_VARSEL (uuid, person_ident, utsendt_tidspunkt, utbetaling_id, sykmelding_id)
+            VALUES (:uuid, :person_ident, :utsendt_tidspunkt, :utbetaling_id, :sykmelding_id)
+            """.trimIndent()
 
         val utsendtVarselUUID = UUID.randomUUID()
-        val parameters = mapOf(
-            "uuid" to utsendtVarselUUID,
-            "person_ident" to personIdent,
-            "utsendt_tidspunkt" to LocalDate.now(),
-            "utbetaling_id" to utbetalingId,
-            "sykmelding_id" to sykmeldingId,
-        )
+        val parameters =
+            mapOf(
+                "uuid" to utsendtVarselUUID,
+                "person_ident" to personIdent,
+                "utsendt_tidspunkt" to LocalDateTime.now(),
+                "utbetaling_id" to utbetalingId,
+                "sykmelding_id" to sykmeldingId,
+            )
 
         namedParameterJdbcTemplate.update(sql, parameters)
         return utsendtVarselUUID
@@ -53,11 +61,13 @@ class VarselRepository(
         val sql = """
             SELECT spdi.person_ident, spdi.utbetaling_id, sykmelding.sykmelding_id FROM sykepengedager_informasjon spdi
             LEFT JOIN UTSENDT_VARSEL utsendt_varsel ON spdi.utbetaling_id = utsendt_varsel.utbetaling_id
+            LEFT JOIN COPY_UTSENDT_VARSEL_ESYFOVARSEL ON spdi.person_ident = COPY_UTSENDT_VARSEL_ESYFOVARSEL.fnr
             JOIN SYKMELDING sykmelding ON spdi.person_ident = sykmelding.employee_identification_number
             WHERE spdi.gjenstaende_sykedager < $gjenstaendeSykedagerLimit
             AND spdi.FORELOPIG_BEREGNET_SLUTT >= current_date + INTERVAL '$maxDateLimit' DAY
             AND sykmelding.tom > CURRENT_TIMESTAMP
-            AND (utsendt_varsel.UTSENDT_TIDSPUNKT IS NULL OR utsendt_varsel.UTSENDT_TIDSPUNKT <= NOW() - INTERVAL '$nyttVarselLimit' DAY);
+            AND (utsendt_varsel.UTSENDT_TIDSPUNKT IS NULL OR utsendt_varsel.UTSENDT_TIDSPUNKT <= NOW() - INTERVAL '$nyttVarselLimit' DAY)
+            AND (COPY_UTSENDT_VARSEL_ESYFOVARSEL.UTSENDT_TIDSPUNKT IS NULL OR COPY_UTSENDT_VARSEL_ESYFOVARSEL.UTSENDT_TIDSPUNKT <= NOW() - INTERVAL '$nyttVarselLimit' DAY); 
             """
 
         return namedParameterJdbcTemplate.query(sql, MerOppfolgingVarselDTOMapper())
@@ -65,23 +75,27 @@ class VarselRepository(
 }
 
 internal class UtsendtVarselRowMapper : RowMapper<UtsendtVarsel> {
-    override fun mapRow(rs: ResultSet, rowNum: Int): UtsendtVarsel {
-        return UtsendtVarsel(
+    override fun mapRow(
+        rs: ResultSet,
+        rowNum: Int,
+    ): UtsendtVarsel =
+        UtsendtVarsel(
             uuid = UUID.fromString(rs.getString("uuid")),
             personIdent = rs.getString("person_ident"),
             utsendtTidspunkt = rs.getTimestamp("utsendt_tidspunkt").toLocalDateTime(),
             utbetalingId = rs.getString("utbetaling_id"),
             sykmeldingId = rs.getString("sykmelding_id"),
         )
-    }
 }
 
 internal class MerOppfolgingVarselDTOMapper : RowMapper<MerOppfolgingVarselDTO> {
-    override fun mapRow(rs: ResultSet, rowNum: Int): MerOppfolgingVarselDTO {
-        return MerOppfolgingVarselDTO(
+    override fun mapRow(
+        rs: ResultSet,
+        rowNum: Int,
+    ): MerOppfolgingVarselDTO =
+        MerOppfolgingVarselDTO(
             personIdent = rs.getString("person_ident"),
             utbetalingId = rs.getString("utbetaling_id"),
             sykmeldingId = rs.getString("sykmelding_id"),
         )
-    }
 }
