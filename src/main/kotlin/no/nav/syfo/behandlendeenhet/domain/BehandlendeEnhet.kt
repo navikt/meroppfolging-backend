fca594cc0bcdbcaa0066135922cd189d79bb1cf6
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-10-24 10:50:32 +0200

    wip

--- src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/BehandlendeEnhet.kt
+++ src/main/kotlin/no/nav/syfo/behandlendeenhet/domain/BehandlendeEnhet.kt
@@ -4,6 +4,8 @@ const val OVRE_EIKER_ENHETSNUMMER = "0624"
 const val ASKER_ENHETSNUMMER = "0220"
 const val NORDRE_AKER_ENHETSNUMMER = "0331"
 const val PROD_GCP = "prod-gcp"
+const val TEST_ENHETSNUMMER = "0314"
+const val TEST_ENHETSNUMMER_2 = "1203"
 
 data class BehandlendeEnhet(
     var enhetId: String,
@@ -16,5 +18,5 @@ fun BehandlendeEnhet.isPilot(clusterName: String): Boolean {
         return listOf(OVRE_EIKER_ENHETSNUMMER, ASKER_ENHETSNUMMER, NORDRE_AKER_ENHETSNUMMER).contains(this.enhetId)
     }
 
-    return true
+    return (listOf(TEST_ENHETSNUMMER, TEST_ENHETSNUMMER_2, "1037", "1742", "0105", "1532").contains(this.enhetId))
 }
