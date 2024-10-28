d187a4f654bc15435c4371f9a20e50389b90b018
malazalkoj <malaz.alkoj@nav.no>
2024-10-28 11:23:11 +0100

    Fix tests after rebasing with main.

--- src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
+++ src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
@@ -4,7 +4,7 @@ import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
 import java.sql.ResultSet
-import java.time.LocalDateTime
+import java.time.LocalDate
 import java.util.*
 
 @Repository
