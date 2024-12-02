4c006875792a37c05cc85b93ecea271012a2fa43
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-12-02 15:05:09 +0100

    refactor senoppfolging controller to use service (#166)

---
+++ src/main/kotlin/no/nav/syfo/senoppfolging/service/SenOppfolgingService.kt
@@ -0,0 +1,203 @@
+package no.nav.syfo.senoppfolging.service
+
+import no.nav.syfo.besvarelse.database.ResponseDao
+import no.nav.syfo.besvarelse.database.domain.FormResponse
+import no.nav.syfo.besvarelse.database.domain.FormType
+import no.nav.syfo.dokarkiv.DokarkivClient
+import no.nav.syfo.domain.PersonIdentNumber
+import no.nav.syfo.logger
+import no.nav.syfo.metric.Metric
+import no.nav.syfo.oppfolgingstilfelle.IsOppfolgingstilfelleClient
+import no.nav.syfo.oppfolgingstilfelle.Oppfolgingstilfelle
+import no.nav.syfo.senoppfolging.kafka.KSenOppfolgingSvarDTO
+import no.nav.syfo.senoppfolging.kafka.SenOppfolgingSvarKafkaProducer
+import no.nav.syfo.senoppfolging.v2.domain.ResponseStatus
+import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingDTOV2
+import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingStatusDTOV2
+import no.nav.syfo.senoppfolging.v2.domain.behovForOppfolging
+import no.nav.syfo.senoppfolging.v2.domain.toQuestionResponse
+import no.nav.syfo.senoppfolging.v2.domain.toResponseStatus
+import no.nav.syfo.syfoopppdfgen.PdfgenService
+import no.nav.syfo.sykepengedagerinformasjon.domain.forelopigBeregnetSluttFormatted
+import no.nav.syfo.sykepengedagerinformasjon.service.SykepengedagerInformasjonService
+import no.nav.syfo.varsel.Varsel
+import no.nav.syfo.varsel.VarselService
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+@Service
+class SenOppfolgingService(
+    val sykepengedagerInformasjonService: SykepengedagerInformasjonService,
+    val isOppfolgingstilfelleClient: IsOppfolgingstilfelleClient,
+    val varselService: VarselService,
+    val responseDao: ResponseDao,
+    val metric: Metric,
+    val senOppfolgingSvarKafkaProducer: SenOppfolgingSvarKafkaProducer,
+    val dokarkivClient: DokarkivClient,
+    val syfoopfpdfgenService: PdfgenService,
+) {
+    private val log = logger()
+
+    fun prepareAndBuildSenOppfolgingStatusDTOV2(
+        personIdent: String,
+        token: String,
+    ): SenOppfolgingStatusDTOV2 {
+        val sykepengerInformasjon =
+            sykepengedagerInformasjonService.fetchSykepengedagerInformasjonByIdent(personIdent)
+
+        val userAccess = getUserAccess(personIdent, token)
+        val hasAccess = userAccess is UserAccess.Good
+        val varsel = (userAccess as? UserAccess.Good)?.varsel
+        val response = getResponseOrNull(varsel, personIdent)
+
+        return SenOppfolgingStatusDTOV2(
+            responseStatus = response?.questionResponses?.toResponseStatus() ?: ResponseStatus.NO_RESPONSE,
+            response = response?.questionResponses,
+            responseTime = response?.createdAt?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
+            maxDate = sykepengerInformasjon?.forelopigBeregnetSluttFormatted(),
+            gjenstaendeSykedager = sykepengerInformasjon?.gjenstaendeSykedager,
+            hasAccessToSenOppfolging = hasAccess,
+        )
+    }
+
+    fun handleSubitForm(
+        personIdent: String,
+        senOppfolgingForm: SenOppfolgingDTOV2,
+        varsel: Varsel,
+    ) {
+        countMetricsForSvarBeforeProcessing(senOppfolgingForm)
+
+        val createdAt = LocalDateTime.now()
+        val id =
+            responseDao.saveFormResponse(
+                personIdent = PersonIdentNumber(personIdent),
+                questionResponses = senOppfolgingForm.senOppfolgingFormV2.map { it.toQuestionResponse() },
+                formType = FormType.SEN_OPPFOLGING_V2,
+                createdAt = createdAt,
+                utsendtVarselUUID = varsel.uuid,
+            )
+
+        varselService.ferdigstillMerOppfolgingVarsel(personIdent)
+
+        generateAndSendPDFToDokarkiv(personIdent, senOppfolgingForm, id)
+
+        publishSenOppfolgingSvarToKafka(id, personIdent, createdAt, senOppfolgingForm, varsel)
+
+        countMetricsForSvarAfterProcessing(senOppfolgingForm)
+    }
+
+    fun getUserAccess(
+        personIdent: String,
+        token: String,
+    ): UserAccess {
+        val oppfolgingstilfelle = isOppfolgingstilfelleClient.getOppfolgingstilfeller(token)
+        val varsel = varselService.getUtsendtVarsel(personIdent)
+
+        return createUserAccess(varsel, oppfolgingstilfelle)
+    }
+
+    fun getResponseOrNull(
+        varsel: Varsel?,
+        personIdent: String,
+    ): FormResponse? =
+        // run block is necessary as long as users with utsendt varsel has response without varsel id
+        varsel?.let {
+            responseDao.findResponseByVarselId(it.uuid)
+                ?: responseDao.findLatestFormResponse(
+                    PersonIdentNumber(personIdent),
+                    FormType.SEN_OPPFOLGING_V2,
+                    varsel.utsendtTidspunkt.toLocalDate(),
+                )
+        }
+
+    private fun generateAndSendPDFToDokarkiv(
+        personident: String,
+        formResponse: SenOppfolgingDTOV2,
+        id: UUID,
+    ) {
+        val pdf = syfoopfpdfgenService.getSenOppfolgingReceiptPdf(personident, formResponse.senOppfolgingFormV2)
+        if (pdf == null) {
+            log.error("Failed to generate PDF")
+        } else {
+            log.info("Generated PDF")
+            dokarkivClient.postDocumentToDokarkiv(
+                fnr = personident,
+                pdf = pdf,
+                uuid = id.toString(),
+                title = "Snart slutt på sykepenger - Kvittering på ditt svar",
+                filnavnBeforeUUID = "SSPS-kvittering",
+            )
+        }
+    }
+
+    private fun publishSenOppfolgingSvarToKafka(
+        id: UUID,
+        personident: String,
+        createdAt: LocalDateTime,
+        formResponse: SenOppfolgingDTOV2,
+        utsendtVarsel: Varsel,
+    ) {
+        senOppfolgingSvarKafkaProducer
+            .publishResponse(
+                KSenOppfolgingSvarDTO(
+                    id = id,
+                    personIdent = personident,
+                    createdAt = createdAt,
+                    response = formResponse.senOppfolgingFormV2,
+                    varselId = utsendtVarsel.uuid,
+                ),
+            )
+    }
+
+    private fun countMetricsForSvarBeforeProcessing(formResponse: SenOppfolgingDTOV2) {
+        if (formResponse.senOppfolgingFormV2.behovForOppfolging()) {
+            metric.countSenOppfolgingRequestYes()
+        } else {
+            metric.countSenOppfolgingRequestNo()
+        }
+    }
+
+    private fun countMetricsForSvarAfterProcessing(formResponse: SenOppfolgingDTOV2) {
+        if (formResponse.senOppfolgingFormV2.behovForOppfolging()) {
+            metric.countSenOppfolgingV2SubmittedYes()
+        } else {
+            metric.countSenOppfolgingV2SubmittedNo()
+        }
+    }
+}
+
+private fun List<Oppfolgingstilfelle>.isInOppfolgingstilfellePlus16Days() =
+    this.firstOrNull()?.let {
+        LocalDate.now().isBefore(it.end.plusDays(17))
+    } ?: false
+
+enum class UserAccessError {
+    NoUtsendtVarsel,
+    NoAccessToSenOppfolging,
+}
+
+sealed class UserAccess {
+    data class Good(
+        val varsel: Varsel,
+    ) : UserAccess()
+
+    data class Error(
+        val error: UserAccessError,
+    ) : UserAccess()
+}
+
+private fun createUserAccess(
+    varsel: Varsel?,
+    oppfolgingstilfelle: List<Oppfolgingstilfelle>,
+): UserAccess {
+    if (!oppfolgingstilfelle.isInOppfolgingstilfellePlus16Days()) {
+        return UserAccess.Error(UserAccessError.NoAccessToSenOppfolging)
+    }
+    if (varsel == null) {
+        return UserAccess.Error(UserAccessError.NoUtsendtVarsel)
+    }
+    return UserAccess.Good(varsel)
+}
--- src/main/kotlin/no/nav/syfo/senoppfolging/v2/SenOppfolgingControllerV2.kt
+++ src/main/kotlin/no/nav/syfo/senoppfolging/v2/SenOppfolgingControllerV2.kt
@@ -7,31 +7,15 @@ import no.nav.syfo.auth.TokenUtil
 import no.nav.syfo.auth.TokenUtil.TokenIssuer.TOKENX
 import no.nav.syfo.auth.TokenValidator
 import no.nav.syfo.auth.getFnr
-import no.nav.syfo.besvarelse.database.ResponseDao
-import no.nav.syfo.besvarelse.database.domain.FormResponse
-import no.nav.syfo.besvarelse.database.domain.FormType
-import no.nav.syfo.dokarkiv.DokarkivClient
-import no.nav.syfo.domain.PersonIdentNumber
 import no.nav.syfo.logger
-import no.nav.syfo.metric.Metric
-import no.nav.syfo.oppfolgingstilfelle.IsOppfolgingstilfelleClient
-import no.nav.syfo.oppfolgingstilfelle.Oppfolgingstilfelle
-import no.nav.syfo.senoppfolging.AlreadyRespondedException
-import no.nav.syfo.senoppfolging.NoAccessToSenOppfolgingException
-import no.nav.syfo.senoppfolging.NoUtsendtVarselException
-import no.nav.syfo.senoppfolging.kafka.KSenOppfolgingSvarDTO
-import no.nav.syfo.senoppfolging.kafka.SenOppfolgingSvarKafkaProducer
-import no.nav.syfo.senoppfolging.v2.domain.ResponseStatus
+import no.nav.syfo.senoppfolging.exception.AlreadyRespondedException
+import no.nav.syfo.senoppfolging.exception.NoAccessToSenOppfolgingException
+import no.nav.syfo.senoppfolging.exception.NoUtsendtVarselException
+import no.nav.syfo.senoppfolging.service.SenOppfolgingService
+import no.nav.syfo.senoppfolging.service.UserAccess
+import no.nav.syfo.senoppfolging.service.UserAccessError
 import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingDTOV2
 import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingStatusDTOV2
-import no.nav.syfo.senoppfolging.v2.domain.behovForOppfolging
-import no.nav.syfo.senoppfolging.v2.domain.toQuestionResponse
-import no.nav.syfo.senoppfolging.v2.domain.toResponseStatus
-import no.nav.syfo.syfoopppdfgen.PdfgenService
-import no.nav.syfo.sykepengedagerinformasjon.domain.forelopigBeregnetSluttFormatted
-import no.nav.syfo.sykepengedagerinformasjon.service.SykepengedagerInformasjonService
-import no.nav.syfo.varsel.Varsel
-import no.nav.syfo.varsel.VarselService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
@@ -40,10 +24,6 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
-import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.UUID
 
 @RestController
 @RequestMapping("/api/v2/senoppfolging")
@@ -54,14 +34,7 @@ class SenOppfolgingControllerV2(
     @Value("\${ESYFO_PROXY_CLIENT_ID}")
     val esyfoProxyClientId: String,
     val tokenValidationContextHolder: TokenValidationContextHolder,
-    val varselService: VarselService,
-    val metric: Metric,
-    val responseDao: ResponseDao,
-    val sykepengedagerInformasjonService: SykepengedagerInformasjonService,
-    val senOppfolgingSvarKafkaProducer: SenOppfolgingSvarKafkaProducer,
-    val dokarkivClient: DokarkivClient,
-    val syfoopfpdfgenService: PdfgenService,
-    val isOppfolgingstilfelleClient: IsOppfolgingstilfelleClient,
+    val senOppfolgingService: SenOppfolgingService,
 ) {
     lateinit var tokenValidator: TokenValidator
     private val log = logger()
@@ -76,30 +49,9 @@ class SenOppfolgingControllerV2(
     @ResponseBody
     fun status(): SenOppfolgingStatusDTOV2 {
         val personIdent = tokenValidator.validateTokenXClaims().getFnr()
-        val sykepengerInformasjon = sykepengedagerInformasjonService.fetchSykepengedagerInformasjonByIdent(
-            personIdent
-        )
-
         val token = TokenUtil.getIssuerToken(tokenValidationContextHolder, TOKENX)
 
-        val oppfolgingstilfelle = isOppfolgingstilfelleClient.getOppfolgingstilfeller(token)
-        val varsel = varselService.getUtsendtVarsel(personIdent)
-        val hasAccess =
-            when (createUserAccess(varsel, oppfolgingstilfelle)) {
-                is UserAccess.Good -> true
-                is UserAccess.Error -> false
-            }
-
-        val response = varsel.getResponseOrNull()
-
-        return SenOppfolgingStatusDTOV2(
-            responseStatus = response?.questionResponses?.toResponseStatus() ?: ResponseStatus.NO_RESPONSE,
-            response = response?.questionResponses,
-            responseTime = response?.createdAt?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")),
-            maxDate = sykepengerInformasjon?.forelopigBeregnetSluttFormatted(),
-            gjenstaendeSykedager = sykepengerInformasjon?.gjenstaendeSykedager,
-            hasAccessToSenOppfolging = hasAccess,
-        )
+        return senOppfolgingService.prepareAndBuildSenOppfolgingStatusDTOV2(personIdent, token)
     }
 
     @PostMapping("/submitform")
@@ -107,156 +59,34 @@ class SenOppfolgingControllerV2(
     fun submitForm(
         @RequestBody senOppfolgingSvar: SenOppfolgingDTOV2,
     ) {
-        countMetricsForSvarBeforeProcessing(senOppfolgingSvar)
-
-        val personident = tokenValidator.validateTokenXClaims().getFnr()
-
+        val personIdent = tokenValidator.validateTokenXClaims().getFnr()
         val token = TokenUtil.getIssuerToken(tokenValidationContextHolder, TOKENX)
-        val oppfolgingstilfelle = isOppfolgingstilfelleClient.getOppfolgingstilfeller(token)
-        val varsel = varselService.getUtsendtVarsel(personident)
-        val utsendtVarsel = validateVarselAndAccess(varsel, oppfolgingstilfelle)
 
-        varsel.getResponseOrNull()?.let {
+        val userAccess = senOppfolgingService.getUserAccess(personIdent, token)
+
+        val varsel =
+            when (userAccess) {
+                is UserAccess.Good -> userAccess.varsel
+                is UserAccess.Error -> {
+                    when (userAccess.error) {
+                        UserAccessError.NoUtsendtVarsel -> throw NoUtsendtVarselException().also {
+                            log.error("User has no valid varsel.")
+                        }
+                        UserAccessError.NoAccessToSenOppfolging -> throw NoAccessToSenOppfolgingException().also {
+                            log.error("User is not in a oppfolgingtilfelle + 16 days.")
+                        }
+                    }
+                }
+            }
+        senOppfolgingService.getResponseOrNull(varsel, personIdent)?.let {
             log.error("User has already responded in the last 3 months.")
             throw AlreadyRespondedException()
         }
 
-        val createdAt = LocalDateTime.now()
-        val id =
-            responseDao.saveFormResponse(
-                personIdent = PersonIdentNumber(personident),
-                questionResponses = senOppfolgingSvar.senOppfolgingFormV2.map { it.toQuestionResponse() },
-                formType = FormType.SEN_OPPFOLGING_V2,
-                createdAt = createdAt,
-                utsendtVarselUUID = utsendtVarsel.uuid,
-            )
-
-        varselService.ferdigstillMerOppfolgingVarsel(personident)
-
-        generateAndSendPDFToDokarkiv(personident, senOppfolgingSvar, id)
-
-        publishSenOppfolgingSvarToKafka(id, personident, createdAt, senOppfolgingSvar, utsendtVarsel)
-
-        countMetricsForSvarAfterProcessing(senOppfolgingSvar)
+        senOppfolgingService.handleSubitForm(
+            personIdent = personIdent,
+            senOppfolgingForm = senOppfolgingSvar,
+            varsel = varsel,
+        )
     }
-
-    private fun validateVarselAndAccess(
-        varsel: Varsel?,
-        oppfolgingstilfelle: List<Oppfolgingstilfelle>,
-    ): Varsel {
-        val userData = createUserAccess(varsel, oppfolgingstilfelle)
-
-        return when (userData) {
-            is UserAccess.Good -> userData.varsel
-            is UserAccess.Error -> {
-                when (userData.error) {
-                    UserDataError.NoUtsendtVarsel -> throw NoUtsendtVarselException().also {
-                        log.error("User has no valid varsel.")
-                    }
-                    UserDataError.NoAccessToSenOppfolging -> throw NoAccessToSenOppfolgingException().also {
-                        log.error("User is not in a oppfolgingtilfelle + 16 days.")
-                    }
-                }
-            }
-        }
-    }
-
-    private fun generateAndSendPDFToDokarkiv(
-        personident: String,
-        formResponse: SenOppfolgingDTOV2,
-        id: UUID,
-    ) {
-        val pdf = syfoopfpdfgenService.getSenOppfolgingReceiptPdf(personident, formResponse.senOppfolgingFormV2)
-        if (pdf == null) {
-            log.error("Failed to generate PDF")
-        } else {
-            log.info("Generated PDF")
-            dokarkivClient.postDocumentToDokarkiv(
-                fnr = personident,
-                pdf = pdf,
-                uuid = id.toString(),
-                title = "Snart slutt på sykepenger - Kvittering på ditt svar",
-                filnavnBeforeUUID = "SSPS-kvittering",
-            )
-        }
-    }
-
-    private fun publishSenOppfolgingSvarToKafka(
-        id: UUID,
-        personident: String,
-        createdAt: LocalDateTime,
-        formResponse: SenOppfolgingDTOV2,
-        utsendtVarsel: Varsel,
-    ) {
-        senOppfolgingSvarKafkaProducer
-            .publishResponse(
-                KSenOppfolgingSvarDTO(
-                    id = id,
-                    personIdent = personident,
-                    createdAt = createdAt,
-                    response = formResponse.senOppfolgingFormV2,
-                    varselId = utsendtVarsel.uuid,
-                ),
-            )
-    }
-
-    private fun countMetricsForSvarBeforeProcessing(formResponse: SenOppfolgingDTOV2) {
-        if (formResponse.senOppfolgingFormV2.behovForOppfolging()) {
-            metric.countSenOppfolgingRequestYes()
-        } else {
-            metric.countSenOppfolgingRequestNo()
-        }
-    }
-
-    private fun countMetricsForSvarAfterProcessing(formResponse: SenOppfolgingDTOV2) {
-        if (formResponse.senOppfolgingFormV2.behovForOppfolging()) {
-            metric.countSenOppfolgingV2SubmittedYes()
-        } else {
-            metric.countSenOppfolgingV2SubmittedNo()
-        }
-    }
-
-    private fun Varsel?.getResponseOrNull(): FormResponse? =
-        // run block is necessary as long as users with utsendt varsel has response without varsel id
-        this?.let {
-            responseDao.findResponseByVarselId(it.uuid)
-                ?: responseDao.findLatestFormResponse(
-                    PersonIdentNumber(personIdent),
-                    FormType.SEN_OPPFOLGING_V2,
-                    utsendtTidspunkt.toLocalDate(),
-                )
-        }
-}
-
-private fun List<Oppfolgingstilfelle>.isInOppfolgingstilfellePlus16Days() =
-    this.firstOrNull()?.let {
-        LocalDate.now().isBefore(it.end.plusDays(17))
-    } ?: false
-
-private enum class UserDataError {
-    NoUtsendtVarsel,
-    NoAccessToSenOppfolging,
-}
-
-private sealed class UserAccess {
-    data class Good(
-        val varsel: Varsel,
-    ) : UserAccess()
-
-    data class Error(
-        val error: UserDataError,
-    ) : UserAccess()
-}
-
-private fun createUserAccess(
-    varsel: Varsel?,
-    oppfolgingstilfelle: List<Oppfolgingstilfelle>,
-): UserAccess {
-    if (!oppfolgingstilfelle.isInOppfolgingstilfellePlus16Days()) {
-        return UserAccess.Error(UserDataError.NoAccessToSenOppfolging)
-    }
-    if (varsel == null) {
-        return UserAccess.Error(UserDataError.NoUtsendtVarsel)
-    }
-    return UserAccess.Good(varsel)
 }
