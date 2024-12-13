b9a678eac192e7824026a798e435b3174c1f1eb6
Tanja Krasnomovets <Vitenok@users.noreply.github.com>
2024-12-13 11:10:22 +0100

    Add catch PSQLException (#174)
    * Add catch PSQLException
    
    * Supress SwallowedException

--- src/main/kotlin/no/nav/syfo/sykepengedagerinformasjon/service/SykepengedagerInformasjonService.kt
+++ src/main/kotlin/no/nav/syfo/sykepengedagerinformasjon/service/SykepengedagerInformasjonService.kt
@@ -4,15 +4,21 @@ import no.nav.syfo.logger
 import no.nav.syfo.sykepengedagerinformasjon.database.SykepengedagerInformasjonDAO
 import no.nav.syfo.sykepengedagerinformasjon.domain.PSykepengedagerInformasjon
 import no.nav.syfo.sykepengedagerinformasjon.domain.SykepengedagerInformasjonDTO
+import org.postgresql.util.PSQLException
 import org.springframework.stereotype.Service
 
 @Service
+@Suppress("SwallowedException")
 class SykepengedagerInformasjonService(private val sykepengedagerInformasjonDAO: SykepengedagerInformasjonDAO) {
     private val log = logger()
 
     fun processSykepengeDagerRecord(sykepengeDagerDTO: SykepengedagerInformasjonDTO) {
         log.info("[SYKEPENGEDAGER_INFO] Persisting sykepengedager-inforasjon record with id: ${sykepengeDagerDTO.id}")
-        sykepengedagerInformasjonDAO.persistSykepengedagerInformasjon(sykepengeDagerDTO)
+        try {
+            sykepengedagerInformasjonDAO.persistSykepengedagerInformasjon(sykepengeDagerDTO)
+        } catch (e: PSQLException) {
+            log.error("Unable to persist record from SPDI with id: ${sykepengeDagerDTO.id} due to PSQLException")
+        }
     }
 
     fun fetchSykepengedagerInformasjonByIdent(personIdent: String): PSykepengedagerInformasjon? {
