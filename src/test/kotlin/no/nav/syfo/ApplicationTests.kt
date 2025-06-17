1f7c72b055ecbe0d0e11a5b05ade297dd3a5b5af
Bjørn Faaberg <bjof@nav.no>
2025-06-17 19:25:09 +0200

    Replace testcontainers with Zonky
    Zonky is an embedded database, that fully supports postgres.

--- src/test/kotlin/no/nav/syfo/ApplicationTests.kt
+++ src/test/kotlin/no/nav/syfo/ApplicationTests.kt
@@ -1,6 +1,9 @@
 package no.nav.syfo
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.RefreshMode.BEFORE_CLASS
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
@@ -8,6 +11,7 @@ import org.springframework.kafka.test.context.EmbeddedKafka
 
 @TestConfiguration
 @EmbeddedKafka
+@AutoConfigureEmbeddedDatabase(provider = ZONKY, refresh = BEFORE_CLASS)
 @SpringBootTest(classes = [LocalApplication::class])
 class ApplicationTests : DescribeSpec() {
     @Suppress("EmptyFunctionBlock")
--- src/test/kotlin/no/nav/syfo/LocalApplication.kt
+++ src/test/kotlin/no/nav/syfo/LocalApplication.kt
@@ -1,32 +1,13 @@
 package no.nav.syfo
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider.ZONKY
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.RefreshMode.BEFORE_CLASS
 import org.springframework.boot.fromApplication
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection
-import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.KafkaContainer
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.utility.DockerImageName
-
+@AutoConfigureEmbeddedDatabase(provider = ZONKY, refresh = BEFORE_CLASS)
 @TestConfiguration(proxyBeanMethods = false)
-class LocalApplication {
-
-    @Bean
-    @ServiceConnection
-    fun kafkaContainer(): KafkaContainer {
-        return KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
-    }
-
-    @Bean
-    @ServiceConnection
-    fun dbContainer(): PostgreSQLContainer<Nothing> {
-        return PostgreSQLContainer<Nothing>(DockerImageName.parse("postgres:17-alpine")).apply {
-            withDatabaseName("testdb")
-            withUsername("test")
-            withPassword("test")
-        }
-    }
-}
+class LocalApplication
 
 fun main(args: Array<String>) {
     fromApplication<Application>().with(LocalApplication::class.java).run(*args)
