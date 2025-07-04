f194af91bde969ab7451b6dd8e629303caf11090
Daniel Engebretsen <danielengebretsen1@gmail.com>
2025-07-04 09:46:19 +0200

    use org.testcontainers.kafka.KafkaContainer (#245)

--- src/test/kotlin/no/nav/syfo/LocalApplication.kt
+++ src/test/kotlin/no/nav/syfo/LocalApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.fromApplication
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.kafka.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
@@ -14,7 +14,7 @@ class LocalApplication {
     @Bean
     @ServiceConnection
     fun kafkaContainer(): KafkaContainer {
-        return KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"))
+        return KafkaContainer(DockerImageName.parse("apache/kafka"))
     }
 
     @Bean
