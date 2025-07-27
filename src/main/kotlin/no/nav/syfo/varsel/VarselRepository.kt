9a3434b88dc6bb1c196f7e92d9e53ba1b5757f78
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-07-27 23:21:07 +0200

    Smarter query
    But it is too slow in current setup

--- src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
+++ src/main/kotlin/no/nav/syfo/varsel/VarselRepository.kt
@@ -11,8 +11,9 @@ import java.util.*
 class VarselRepository(
     private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate,
 ) {
-    val maxDagerIgjenTilMaksdatoForVarsling = 120
-    val minDagerIgjenTilMaksdatoForVarsling = 14
+    val upperBoundDagerIgjenTilMaksdatoForVarsling = 120
+    val lowerBoundDagerIgjenTilMaksdatoForVarsling = 14
+    val maxDaysSinceUtbetalingCreatedForTakingItIntoAccount = 60
     val minDagerMellomToVarslerTilSammePerson = 110
 
     fun getUtsendtVarsel(personIdent: String): UtsendtVarsel? {
@@ -99,35 +100,68 @@ class VarselRepository(
     fun fetchMerOppfolgingVarselToBeSent(): List<MerOppfolgingVarselDTO> {
         val sql =
             """
-            SELECT spdi.utbetaling_id, spdi.person_ident, spdi.gjenstaende_sykedager, spdi.forelopig_beregnet_slutt, sykmelding.sykmelding_id
-            FROM sykepengedager_informasjon AS spdi
-            JOIN SYKMELDING sykmelding ON spdi.person_ident = sykmelding.employee_identification_number
-            WHERE spdi.utbetaling_id =
-                (SELECT spdi2.utbetaling_id
-                FROM sykepengedager_informasjon AS spdi2
-                WHERE spdi.person_ident = spdi2.person_ident
-                ORDER BY utbetaling_created_at DESC
-                LIMIT 1)
-            AND sykmelding.sykmelding_id = 
-                (SELECT latest_sykmelding.sykmelding_id 
-                FROM sykmelding AS latest_sykmelding
-                WHERE sykmelding.employee_identification_number = latest_sykmelding.employee_identification_number
-                ORDER BY latest_sykmelding.created_at DESC
-                LIMIT 1)
-            AND spdi.forelopig_beregnet_slutt <= current_date + INTERVAL '$maxDagerIgjenTilMaksdatoForVarsling' DAY
-            AND spdi.forelopig_beregnet_slutt >= current_date + INTERVAL '$minDagerIgjenTilMaksdatoForVarsling' DAY
-            AND sykmelding.tom > CURRENT_TIMESTAMP
-            AND spdi.person_ident NOT IN
-                (SELECT utsendt_varsel.person_ident
-                FROM UTSENDT_VARSEL
-                WHERE UTSENDT_TIDSPUNKT > NOW() - INTERVAL '$minDagerMellomToVarslerTilSammePerson' DAY)
-            AND spdi.person_ident NOT IN
-                (SELECT copy_utsendt_varsel_esyfovarsel.fnr
-                FROM copy_utsendt_varsel_esyfovarsel
-                WHERE UTSENDT_TIDSPUNKT > NOW() - INTERVAL '$minDagerMellomToVarslerTilSammePerson' DAY)
-            AND spdi.person_ident NOT IN
-                (SELECT skip_varselutsending.person_ident
-                FROM skip_varselutsending);
+            WITH filtrerte_utbetalinger AS (
+                SELECT spdi.utbetaling_id,
+                       spdi.person_ident,
+                       spdi.gjenstaende_sykedager,
+                       spdi.forelopig_beregnet_slutt,
+                       spdi.utbetaling_created_at,
+                       sykmelding.sykmelding_id
+                FROM sykepengedager_informasjon AS spdi
+                         JOIN SYKMELDING sykmelding ON spdi.person_ident = sykmelding.employee_identification_number
+                -- only consider latest utbetaling
+                WHERE spdi.utbetaling_id =
+                      (SELECT spdi2.utbetaling_id
+                       FROM sykepengedager_informasjon AS spdi2
+                       WHERE spdi.person_ident = spdi2.person_ident
+                       ORDER BY utbetaling_created_at DESC
+                       LIMIT 1)
+                  -- only consider latest sykmelding
+                  AND sykmelding.sykmelding_id =
+                      (SELECT latest_sykmelding.sykmelding_id
+                       FROM sykmelding AS latest_sykmelding
+                       WHERE sykmelding.employee_identification_number = latest_sykmelding.employee_identification_number
+                       ORDER BY latest_sykmelding.created_at DESC
+                       LIMIT 1)
+                  -- person must be currently sykmeldt
+                  AND sykmelding.tom > CURRENT_TIMESTAMP
+                  -- check that we are within range of maksdato for the latest utbetaling
+                  AND spdi.forelopig_beregnet_slutt <= current_date + INTERVAL '120' DAY
+                  AND spdi.forelopig_beregnet_slutt >= current_date + INTERVAL '14' DAY
+                  -- check that the utbetaling is not too old to consider, unless it has a low number of gjenstaende_sykedager
+                  AND (spdi.utbetaling_created_at >= NOW() - INTERVAL '60' DAY
+                    OR
+                       spdi.gjenstaende_sykedager < 85)
+                  -- check that we have waited long enough since the person last got a SSPS-varsel
+                  AND spdi.person_ident NOT IN
+                      (SELECT utsendt_varsel.person_ident
+                       FROM UTSENDT_VARSEL
+                       WHERE UTSENDT_TIDSPUNKT > NOW() - INTERVAL '110' DAY)
+                  AND spdi.person_ident NOT IN
+                      (SELECT copy_utsendt_varsel_esyfovarsel.fnr
+                       FROM copy_utsendt_varsel_esyfovarsel
+                       WHERE UTSENDT_TIDSPUNKT > NOW() - INTERVAL '110' DAY)
+                  -- check if we should skip sending a varsel to the person
+                  AND spdi.person_ident NOT IN
+                      (SELECT skip_varselutsending.person_ident
+                       FROM skip_varselutsending)
+            )
+            SELECT *
+            FROM filtrerte_utbetalinger LEFT JOIN LATERAL (
+                SELECT employee_identification_number,
+                       -- count number of sykmelding days since utbetaling_created_at
+                       SUM((tom::date - GREATEST(fom, filtrerte_utbetalinger.utbetaling_created_at)::date)::int + 1)
+                           AS sykmelding_days
+                FROM sykmelding
+                GROUP BY employee_identification_number) sd
+            ON filtrerte_utbetalinger.person_ident = sd.employee_identification_number
+            WHERE NOT (
+                -- If there is more than a month since the utbetaling was created, check that the person has been
+                -- sykmeldt more than half of the days since the utbetaling was created.
+                filtrerte_utbetalinger.utbetaling_created_at < (CURRENT_DATE - INTERVAL '1 month')
+                    AND COALESCE(sd.sykmelding_days, 0) <
+                        (DATE_PART('day', CURRENT_DATE - filtrerte_utbetalinger.utbetaling_created_at) / 2)
+                );
             """.trimIndent()
 
         return namedParameterJdbcTemplate.query(sql, MerOppfolgingVarselDTOMapper())
