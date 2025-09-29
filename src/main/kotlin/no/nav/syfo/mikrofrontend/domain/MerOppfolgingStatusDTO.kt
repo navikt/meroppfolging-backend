4e18e748fd4ff0016c7b3e300c1614003347fbc1
Audun Sørheim <audun.sorheim@nav.no>
2025-09-29 11:54:52 +0200

    wip

--- src/main/kotlin/no/nav/syfo/mikrofrontend/domain/MerOppfolgingStatusDTO.kt
+++ src/main/kotlin/no/nav/syfo/mikrofrontend/domain/MerOppfolgingStatusDTO.kt
@@ -1,37 +1,59 @@
 package no.nav.syfo.mikrofrontend.domain
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import no.nav.syfo.kartlegging.domain.PersistedKartleggingssporsmal
 import no.nav.syfo.senoppfolging.v2.domain.SenOppfolgingStatusDTOV2
+import java.time.format.DateTimeFormatter
 
 sealed class MerOppfolgingStatusDTO {
     abstract val oppfolgingsType: OppfolgingsType
-    abstract val senOppfolgingStatus: SenOppfolgingStatusDTOV2?
 
     data class SenOppfolging(
         @field:JsonInclude(JsonInclude.Include.ALWAYS)
-        override val senOppfolgingStatus: SenOppfolgingStatusDTOV2,
+        val senOppfolgingStatus: SenOppfolgingStatusDTOV2,
     ) : MerOppfolgingStatusDTO() {
         override val oppfolgingsType: OppfolgingsType = OppfolgingsType.SEN_OPPFOLGING
     }
 
     data class Kartlegging(
-        val hasSubmittedKartlegging: Boolean,
         @field:JsonInclude(JsonInclude.Include.ALWAYS)
-        override val senOppfolgingStatus: SenOppfolgingStatusDTOV2? = null,
+        val kartleggingStatus: KartleggingStatusDTO,
     ) : MerOppfolgingStatusDTO() {
         override val oppfolgingsType: OppfolgingsType = OppfolgingsType.KARTLEGGING
     }
 
     data class IngenOppfolging(
         @field:JsonInclude(JsonInclude.Include.ALWAYS)
-        override val senOppfolgingStatus: SenOppfolgingStatusDTOV2? = null,
+        val senOppfolgingStatus: SenOppfolgingStatusDTOV2? = null,
     ) : MerOppfolgingStatusDTO() {
         override val oppfolgingsType: OppfolgingsType = OppfolgingsType.INGEN_OPPFOLGING
     }
-
-    companion object {
-        fun sen(status: SenOppfolgingStatusDTOV2) = SenOppfolging(status)
-        fun kartlegging(hasSubmitted: Boolean) = Kartlegging(hasSubmitted)
-        fun ingen() = IngenOppfolging()
-    }
 }
+
+enum class KartleggingResponseStatusType {
+    NO_RESPONSE,
+    SUBMITTED,
+}
+
+data class KartleggingStatusDTO(
+    val responseStatus: KartleggingResponseStatusType,
+    val responseDateTime: String?,              // ISO timestamp or null
+    val hasAccessToKartlegging: Boolean,
+)
+
+fun PersistedKartleggingssporsmal?.toKartleggingStatusDTO(
+    hasAccessToKartlegging: Boolean,
+): KartleggingStatusDTO =
+    if (this == null) {
+        KartleggingStatusDTO(
+            responseStatus = KartleggingResponseStatusType.NO_RESPONSE,
+            responseDateTime = null,
+            hasAccessToKartlegging = hasAccessToKartlegging,
+        )
+    } else {
+        KartleggingStatusDTO(
+            responseStatus = KartleggingResponseStatusType.SUBMITTED,
+            responseDateTime = DateTimeFormatter.ISO_INSTANT.format(createdAt),
+            hasAccessToKartlegging = hasAccessToKartlegging,
+        )
+    }
