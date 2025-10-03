82765618ef294a61e3d69064cf6d3b961144af41
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-10-03 08:48:46 +0200

    add some logging to the job

--- src/main/kotlin/no/nav/syfo/kartlegging/job/JournalforingJob.kt
+++ src/main/kotlin/no/nav/syfo/kartlegging/job/JournalforingJob.kt
@@ -2,6 +2,7 @@ package no.nav.syfo.kartlegging.job
 
 import no.nav.syfo.kartlegging.service.KartleggingssporsmalService
 import no.nav.syfo.leaderelection.LeaderElectionClient
+import no.nav.syfo.logger
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
@@ -10,16 +11,22 @@ class JournalforingJob(
     private val leaderElectionClient: LeaderElectionClient,
     private val kartleggingssporsmalService: KartleggingssporsmalService,
 ) {
+    private val logger = logger()
+    private val logName = "[${JournalforingJob::class.simpleName}]"
+
 
     @Scheduled(cron = "\${retry.kartlegging.journalfor.job.cron}")
     fun retryJournalforing() {
         if (!leaderElectionClient.isPodLeader()) {
             return
         }
+        logger.info("$logName Starter jobb for å journalføre svar kartleggingsspørsmål")
 
         val kartleggingssporsmalNotJournaled = kartleggingssporsmalService.getKartleggingssporsmalNotJournaled()
+        logger.info("$logName Fant ${kartleggingssporsmalNotJournaled.size} kartleggingsspørsmål som ikke er journalført")
         kartleggingssporsmalNotJournaled.forEach {
             kartleggingssporsmalService.jornalforKartleggingssporsmal(it.uuid, it, it.createdAt)
         }
+        logger.info("$logName Avslutter jobb for å journalføre svar kartleggingsspørsmål")
     }
 }
