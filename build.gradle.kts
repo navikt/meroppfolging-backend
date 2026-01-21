3665f1e629d8bb939c6f9d2fc0a819dc8220a98c
Bjørn Faaberg <bjorn.faaberg@nav.no>
2026-01-21 10:33:36 +0100

    Upgrade patch version of spring boot to 3.5.9 (#314)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.5.7"
+    id("org.springframework.boot") version "3.5.9"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     kotlin("jvm") version "2.2.21"
